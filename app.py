7ba0156cf536098f7fbc0b831db3ea6dd5edbd9e
Sirimaaa <88994976+Sirimaaa@users.noreply.github.com>
2021-11-25 13:29:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def callback():
     no_event = len(decoded['events'])
     for i in range(no_event):
             event = decoded['events'][i]
-            event_handle(event)
+            event_handle(event,json_line)
 
     # เชื่อมต่อกับ dialogflow
     #intent = decoded["queryResult"]["intent"]["displayName"] 
@@ -124,7 +124,7 @@ def reply(intent,text,reply_token,id,disname):
     text_message = TextSendMessage(text="ทดสอบ")
     line_bot_api.reply_message(reply_token,text_message)
 
-def event_handle(event):
+def event_handle(event,json_line):
     print(event)
     try:
         userId = event['source']['userId']
@@ -164,7 +164,7 @@ def event_handle(event):
             headers.items()}) 
             json_headers.update({'Host':'bots.dialogflow
             .com'}) 
-            url = "" 
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/71ab6534-b88a-429c-ab9e-216b20bd53c1" 
             requests.post(url,data=json_line, 
             headers=json_headers)
     elif msgType == "image":
